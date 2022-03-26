6f66d03dadb910b9b256aedee0b83b5078148991
Xmal1 <dnhatlam15@gmail.com>
2022-03-26 23:52:39 +0700

    Add project files.

---
+++ 1911065278_DuongNhatLam_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1911065278_DuongNhatLam_BigSchool.MvcApplication" Language="C#" %>
---
+++ 1911065278_DuongNhatLam_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1911065278_DuongNhatLam_BigSchool.Startup))]
+namespace _1911065278_DuongNhatLam_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
