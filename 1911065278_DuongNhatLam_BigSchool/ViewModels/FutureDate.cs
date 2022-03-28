83e3f13d8ff7c57faae622bfe454f34a75ea5cb1
Xmal1 <dnhatlam15@gmail.com>
2022-03-28 07:41:50 +0700

    nop bai ngay 28/03/2022

--- 1911065278_DuongNhatLam_BigSchool/ViewModels/FutureDate.cs
+++ 1911065278_DuongNhatLam_BigSchool/ViewModels/FutureDate.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace _1911065278_DuongNhatLam_BigSchool.ViewModels
 {
-    public class FutureDate
+    public class FutureDate : ValidationAttribute
     {
+        public override bool IsValid(object value)
+        {
+            return base.IsValid(value);
+        }
     }
 }
